6b87dc9252a1002aa0a27cbaa3a548c462649417
João Pedro Borges Dutra <142460876+BorgesJPBD@users.noreply.github.com>
2024-11-23 23:52:28 -0400

    Update ex15.py

--- ex15.py
+++ ex15.py
@@ -1,50 +1,5 @@
-import random
-
-n1 = int(input("Tente adivinhar o numero que eu escolhi entre 1 e 100:"))
-sorte = random.randint(1,100)
-tentativa = 1 
-
-def adivinhacao(adivinhar):
-    global tentativa
-    while adivinhar != sorte: 
-        if adivinhar > sorte:
-            print ("Esse numero é maior tente denovo")
-        elif adivinhar < sorte:
-            print ("Esse numero é menor tente outa vez")
-            
-            
-        adivinhar = int(input(f"Tentativa {tentativa}: Digite outro número: "))
-        tentativa += 1 
-    return adivinhar
-    
-            
-adivinhacao_final = adivinhacao(n1)
-print (adivinhacao_final)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 import random
-
 
 sorte = random.randint(1, 100)
 tentativa = 1
@@ -70,7 +25,7 @@ def adivinhacao(adivinhar):
 adivinhacao_final = adivinhacao(n1)
 
 
-print(f"Parabéns! Você acertou o número {sorte} em {tentativa} tentativas.")
+print(f"Parabéns! Você acertou o número {sorte} em {tentativa} tentativas."
 
 
    
